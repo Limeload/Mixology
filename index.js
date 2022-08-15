fbab9bb1e06423e9c4ec7d80ed9e5c67ec90a274
Shraddha Rao <shraddharaom@gmail.com>
2022-08-16 00:35:43 +0530

    changed card style

--- index.js
+++ index.js
@@ -11,20 +11,19 @@ const drinks = await response.json();
 // Displaying data that will render it into index.html(card)
 const cocktailList = document.querySelector('#cocktail-list');
 
-let card = '';
+let card = '' ;
 drinks.forEach(drink => {
   card += `
   <div class="container-card">
-        <div class="card" style="width: 18rem;">
-            <img src="${drink.strDrinkThumb}" class="card-img-top" alt="">
-            <div class="card-body">
-              <h5 class="card-title"> ${drink.idDrink}</h5>
-              <p class="card-text"> ${drink.strDrink}</p>
-              <ul class="list-group list-group-flush">
-              <li class="list-group-item">${drink.type}</li>
-              <li class="list-group-item">${drink.glass}</li>
-            </ul>
-            </div>
+    <div class="card border-dark" style="width: 18rem;">
+      <img src="${drink.strDrinkThumb}" class="card-img-top" alt="${drink.strDrink}">
+      <div class="card-body">
+        <h5 class="card-title">${drink.idDrink}</h5>
+        <p class="card-text">${drink.strDrink}</p>
+        <h5 class="card-title">${drink.glass}</h5>
+      </div>
+    </div>
+    </div>
   `
 })
 cocktailList.innerHTML = card;
